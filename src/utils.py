9e93fe7c999d378e572d63bf36763353c0d4f563
Yasser Souri <yassersouri@gmail.com>
2012-03-07 00:04:36 +0330

    add utils.py for putting utility functions in it.
    add a function to convert each input image to binary

---
+++ src/utils.py
@@ -0,0 +1,33 @@
+
+import cv
+
+def main():
+	inputFolder = '../data/'
+	inputFile = '002002_002.png'
+
+	img = cv.LoadImageM(inputFolder+inputFile)
+
+	cv.ShowImage("input", img)
+
+	outputImg = toBinary(img)
+
+	cv.ShowImage("output",outputImg)
+
+	cv.WaitKey(0)
+
+def toBinary(img):
+	# Create an image to store the output version on
+	result = cv.CreateImage(cv.GetSize(img), cv.IPL_DEPTH_8U, 1)
+
+	# convert to grayscale
+	cv.CvtColor(img, result, cv.CV_RGB2GRAY)
+
+	# apply threshold
+	# TODO: Make use of better thresholding algorithms
+	thr = 230
+	cv.Threshold(result, result, thr, 255, cv.CV_THRESH_BINARY)
+
+	return result
+
+if __name__ == '__main__':
+	main()

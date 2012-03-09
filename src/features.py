bf87396b57ad9ecfe355d7d5b659415e243b7d83
Yasser Souri <yassersouri@gmail.com>
2012-03-09 23:42:03 +0330

    add function to extract circularity features

--- src/features.py
+++ src/features.py
@@ -1,12 +1,15 @@
 import cv
 import utils
+import math
 
 def main():
 	inputFolder = '../data/'
 	inputFile = '002002_002.png'
 
 	img = cv.LoadImageM(inputFolder+inputFile)
-	basicGlobalFeatures(utils.toBinary(img))
+	BiImg = utils.toBinary(img)
+	print basicGlobalFeatures(BiImg)
+	print circularityFeature(BiImg)
 
 # This functions returns a tuple contaning 
 # width(W), height(H), area(A) and total number of black pixels(T)
@@ -21,6 +24,15 @@ def basicGlobalFeatures(img):
 			if img[y,x] == 0:
 				T += 1
 	return (W,H,A,T)
-
+# This function returns a tuple containing
+# cicularity feature(Ci = A/C), and radius of the cirscle(Srad)
+# refer to the paper mentioned in README for more detailes
+# (Ci, Srad)
+def circularityFeature(img):
+	(W, H) = cv.GetSize(img)
+	A = W*H
+	Ci = (math.pi * (W**2 + H**2)) / float(2 * W * H)
+	Srad = math.sqrt(W**2 + H**2)/2.0
+	return (Ci, Srad)
 if __name__ == '__main__':
 	main()

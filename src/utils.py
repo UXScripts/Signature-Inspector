49fab035f6de94525cba42a28a7d3e507eafca94
Yasser Souri <yassersouri@gmail.com>
2012-03-10 01:21:30 +0330

    add function to extrac meansize of some sample images

--- src/utils.py
+++ src/utils.py
@@ -4,13 +4,15 @@ def main():
 	inputFolder = '../data/'
 	inputFile = '002002_002.png'
 
-	img = cv.LoadImageM(inputFolder+inputFile)
-	cv.ShowImage("input", img)
+	# img = cv.LoadImageM(inputFolder+inputFile)
+	# cv.ShowImage("input", img)
 
-	outputImg = toBinary(img)
-	cv.ShowImage("output",outputImg)
+	# outputImg = toBinary(img)
+	# cv.ShowImage("output",outputImg)
 
-	cv.WaitKey(0)
+	# cv.WaitKey(0)
+
+	print meanSizeOfSamples(inputFolder)
 
 def toBinary(img):
 	# Create an image to store the output version on
@@ -25,6 +27,30 @@ def toBinary(img):
 	cv.Threshold(result, result, thr, 255, cv.CV_THRESH_BINARY)
 
 	return result
+
+# This function returns a tuple
+# Containing mean width(mW) and height(mH) of some samples
+# (mW,mH) both are floats
+# folder argument is the address to the folder containing all the
+# learning samples.
+# For sample naming convention see README
+def meanSizeOfSamples(folder):
+	import glob
+	files = glob.glob(folder + '*_*.png')
+
+	l = len(files)
+
+	tW = 0.0
+	tH = 0.0
+
+	for _file in files:
+		img = cv.LoadImageM(_file)
+		(W, H) = cv.GetSize(img)
+		tW += W
+		tH += H
+	
+	(mW, mH) = (tW/l, tH/l)
+	return (mW, mH)
 
 if __name__ == '__main__':
 	main()

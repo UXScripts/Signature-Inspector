891047392d23ae6df6b70ff865257faf879de9a9
Yasser Souri <yassersouri@gmail.com>
2012-03-11 13:58:48 +0330

    add functions that calculates projections, both horizontally and vertically

--- src/features.py
+++ src/features.py
@@ -8,8 +8,17 @@ def main():
 
 	img = cv.LoadImageM(inputFolder+inputFile)
 	BiImg = utils.toBinary(img)
-	print basicGlobalFeatures(BiImg)
-	print circularityFeature(BiImg)
+	cv.ShowImage("input", img)
+	cv.ShowImage("Biinput", BiImg)
+
+	Pv = verticalProjection(BiImg)
+	print "Vertical Projection"
+	print Pv
+
+	Ph = horizontalProjection(BiImg)
+	print "Horizontal Projection"
+	print Ph
+	cv.WaitKey(0)
 
 # This functions returns a tuple contaning 
 # width(W), height(H), area(A) and total number of black pixels(T)
@@ -35,6 +44,38 @@ def circularityFeature(img):
 	Ci = (4 * W * H) / (math.pi * (W**2 + H**2))
 	Srad = math.sqrt(W**2 + H**2)/2.0
 	return (Ci, Srad)
+
+# This function calculates the vertical projection of an image
+# returns a list of integers, with length of height of the image.
+# Be carefull this function only counts true black pixles with 0 as their grayscale values
+def verticalProjection(img):
+	(W, H) = cv.GetSize(img)
+	Pv = []
+
+	for y in range(H):
+		tB = 0
+		for x in range(W):
+			if img[y, x] == 0: #Black pixle
+				tB += 1
+		Pv.append(tB)
+	
+	return Pv
+
+# This function calculates the horizontal projection of an image
+# returns a list of integers, with length of width of the image.
+# Be carefull this function only counts true black pixles with 0 as their grayscale values
+def horizontalProjection(img):
+	(W, H) = cv.GetSize(img)
+	Ph = []
+
+	for x in range(W):
+		tB = 0
+		for y in range(H):
+			if img[y, x] == 0: #Black pixle
+				tB += 1
+		Ph.append(tB)
+	
+	return Ph
 
 if __name__ == '__main__':
 	main()
